19a64a6581a72bbced42b21016f96078c9a802a5
joshrhughes <josh@mtnpine.us>
2018-02-21 17:44:39 -0700

    getting the app set up

---
+++ index.js
@@ -0,0 +1,16 @@
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+
+// AppRegistry.registerComponent('albums', () => App);
+
+
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => {
+    return (
+        <Text>Some Text</Text>
+    );
+};
+
+AppRegistry.registerComponent('albums', () => App);
