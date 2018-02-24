1e1add166898d4eb4577f380f55a4fd3c3b908d0
joshrhughes <josh@mtnpine.us>
2018-02-23 18:25:37 -0700

    added scrolling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,20 +4,30 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album;
+    const { title, artist, thumbnail_image, image } = album;
+    const { 
+        thumbnailStyle, 
+        headerTextStyle, 
+        headerContentStyle, 
+        thumbnailContainerStyle,
+        imageStyle
+    } = styles;
 
-    return <Card>
-        <CardSection>
-          <View />
-            <Image style={styles.thumbnailStyle} 
-            source={{ uri: thumbnail_image }} 
-            />
-          <View style={styles.headerContentStyle}>
-            <Text>{title}</Text>
-            <Text>{artist}</Text>
-          </View>
-        </CardSection>
-      </Card>;
+    return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle} />
+        <Image source={{ uri: thumbnail_image }} style={thumbnailStyle} />
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+    </Card>
+    );
 };
 
 const styles = {
@@ -25,9 +35,23 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
