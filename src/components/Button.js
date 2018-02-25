cbdf18c17782e5d91760c7c2d907fe21344941c3
joshrhughes <josh@mtnpine.us>
2018-02-24 17:50:32 -0700

    added amazon links to albums

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress1 }) => {
     const { buttonStyle, textStyle } = styles;
+
     return (
-         <TouchableOpacity style={buttonStyle}>   
+         <TouchableOpacity onPress={ onPress1 } style={buttonStyle}>   
             <Text style={textStyle}>Click Me</Text>
          </TouchableOpacity>   
     );
